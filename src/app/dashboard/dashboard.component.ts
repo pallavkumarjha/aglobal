dea6c748ae8fdf622c6da342fbf0432dfe76a1fe
Pallav <pallav.jha@selfinindia.com>
2020-07-17 20:54:40 +0530

    fixed styling

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,47 +12,52 @@ import { RoleDialogComponent } from '../role-dialog/role-dialog.component';
 export class DashboardComponent implements OnInit {
 
   ELEMENT_DATA = [
-    { name: 'Home',icon: 'home',supervisor: true, super_admin: true, fleet_manager: false, driver: true,admin: true},
-    { name: 'Trips',icon: 'home',supervisor: true, super_admin: true, fleet_manager: true, driver: true,admin: true},
-    { name: 'Past Trips',icon: 'home',supervisor: true, super_admin: true, fleet_manager: false, driver: true,admin: true},
-    { name: 'Create Trip',icon: 'home',supervisor: true, super_admin: true, fleet_manager: false, driver: true,admin: true},
-    { name: 'Alerts',icon: 'home',supervisor: true, super_admin: true, fleet_manager: false, driver: true,admin: true},
-    { name: 'Alert Management',icon: 'home',supervisor: true, super_admin: true, fleet_manager: false, driver: true,admin: true},
-    { name: 'Sensors',icon: 'home',supervisor: true, super_admin: true, fleet_manager: false, driver: true,admin: true},
-    { name: 'Add Sensors',icon: 'home',supervisor: true, super_admin: true, fleet_manager: false, driver: true,admin: true},
-    { name: 'Routes',icon: 'home',supervisor: true, super_admin: true, fleet_manager: false, driver: true,admin: true},
-    { name: 'Add Routes',icon: 'home',supervisor: true, super_admin: true, fleet_manager: false, driver: true,admin: true},
-    { name: 'Users',icon: 'home',supervisor: true, super_admin: true, fleet_manager: false, driver: true,admin: true},
-    { name: 'Add Users',icon: 'home',supervisor: true, super_admin: true, fleet_manager: false, driver: true,admin: true},
-    { name: 'Dashboard',icon: 'home',supervisor: true, super_admin: true, fleet_manager: false, driver: true,admin: true},
-    { name: 'Report',icon: 'home',supervisor: true, super_admin: true, fleet_manager: false, driver: true,admin: true},
-    { name: 'Role Settings',icon: 'home',supervisor: true, super_admin: true, fleet_manager: false, driver: true,admin: true},
+    { id:1, name: 'Home',icon: 'home',supervisor: true, super_admin: true, fleet_manager: false, driver: true,admin: true},
+    { id:2, name: 'Trips',icon: 'home',supervisor: true, super_admin: true, fleet_manager: true, driver: true,admin: true},
+    { id:3, name: 'Past Trips',icon: 'home',supervisor: true, super_admin: true, fleet_manager: false, driver: true,admin: true},
+    { id:4, name: 'Create Trip',icon: 'home',supervisor: true, super_admin: true, fleet_manager: false, driver: true,admin: true},
+    { id:5, name: 'Alerts',icon: 'home',supervisor: true, super_admin: true, fleet_manager: false, driver: true,admin: true},
+    { id:6, name: 'Alert Management',icon: 'home',supervisor: true, super_admin: true, fleet_manager: false, driver: true,admin: true},
+    { id:7, name: 'Sensors',icon: 'home',supervisor: true, super_admin: true, fleet_manager: false, driver: true,admin: true},
+    { id:8, name: 'Add Sensors',icon: 'home',supervisor: true, super_admin: true, fleet_manager: false, driver: true,admin: true},
+    { id:9, name: 'Routes',icon: 'home',supervisor: true, super_admin: true, fleet_manager: false, driver: true,admin: true},
+    { id:10, name: 'Add Routes',icon: 'home',supervisor: true, super_admin: true, fleet_manager: false, driver: true,admin: true},
+    { id:11, name: 'Users',icon: 'home',supervisor: true, super_admin: true, fleet_manager: false, driver: true,admin: true},
+    { id:12, name: 'Add Users',icon: 'home',supervisor: true, super_admin: true, fleet_manager: false, driver: true,admin: true},
+    { id:13, name: 'Dashboard',icon: 'home',supervisor: true, super_admin: true, fleet_manager: false, driver: true,admin: true},
+    { id:14, name: 'Report',icon: 'home',supervisor: true, super_admin: true, fleet_manager: false, driver: true,admin: true},
+    { id:15, name: 'Role Settings',icon: 'home',supervisor: true, super_admin: true, fleet_manager: false, driver: true,admin: true},
   ];
 
   roles = [
     {
       name: 'Supervisor',
       isActive: true,
+      columnName:'supervisor',
       totalAccess: this.getAccessPoints('supervisor')
     },
     {
       name: 'Super Admin',
       isActive: true,
+      columnName:'super_admin',
       totalAccess: this.getAccessPoints('super_admin')
     },
     {
       name: 'Fleet Manager',
       isActive: true,
+      columnName:'fleet_manager',
       totalAccess: this.getAccessPoints('fleet_manager')
     },
     {
       name: 'Driver',
       isActive: true,
+      columnName:'driver',
       totalAccess: this.getAccessPoints('driver')
     },
     {
       name: 'Admin',
       isActive: true,
+      columnName:'admin',
       totalAccess: this.getAccessPoints('admin')
     }
 
@@ -91,12 +96,74 @@ export class DashboardComponent implements OnInit {
   openDialog(): void {
     const dialogRef = this.dialog.open(RoleDialogComponent, {
       width: '700px',
+      maxHeight: '700px'
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed', result);
+      this.transformData(result);
       // this.animal = result;
     });
+  }
+
+  transformData(result) {
+    let roleName = result.name;
+    roleName = roleName.split(' ').join('');
+    this.ELEMENT_DATA.forEach(ele =>{
+      if(ele.id == 1){
+        ele[roleName] = result[1];
+      }
+      if(ele.id == 2){
+        ele[roleName] = result[2];
+      }
+      if(ele.id == 3){
+        ele[roleName] = result[3];
+      }
+      if(ele.id == 4){
+        ele[roleName] = result[4];
+      }
+      if(ele.id == 5){
+        ele[roleName] = result[5];
+      }
+      if(ele.id == 6){
+        ele[roleName] = result[6];
+      }
+      if(ele.id == 7){
+        ele[roleName] = result[7];
+      }
+      if(ele.id == 8){
+        ele[roleName] = result[8];
+      }
+      if(ele.id == 9){
+        ele[roleName] = result[9];
+      }
+      if(ele.id == 10){
+        ele[roleName] = result[10];
+      }
+      if(ele.id == 11){
+        ele[roleName] = result[11];
+      }
+      if(ele.id == 12){
+        ele[roleName] = result[12];
+      }
+      if(ele.id == 13){
+        ele[roleName] = result[13];
+      }
+      if(ele.id == 14){
+        ele[roleName] = result[14];
+      }
+      if(ele.id == 15){
+        ele[roleName] = result[15];
+      }
+    });
+    let obj = {
+      name: `${result.name}`,
+      isActive: (result.isActive == 'true'),
+      columnName: roleName,
+      totalAccess: this.getAccessPoints(roleName),
+    };
+    this.roles.push(obj);
+    this.displayedColumns.push(result.name);
+    console.log(this.ELEMENT_DATA);
   }
 }
 
--- src/app/role-dialog/role-dialog.component.ts
+++ src/app/role-dialog/role-dialog.component.ts
@@ -103,9 +103,30 @@ export class RoleDialogComponent implements OnInit {
   }
 
   submitRoleData(){
-    console.log(this.roleForm.value);
-    console.log(this.accessForm.value);
+    let data = this.transformData(this.roleForm.value, this.accessForm.value);
   }
 
+  transformData(roleForm, accessForm){
+    let data = {
+      name: roleForm.name,
+      isActive: roleForm.isActive.mobile || roleForm.isActive.web,
+      10: accessForm.add_routes || accessForm.add_routes,
+      12: accessForm.add_users.mobile || accessForm.add_users.web,
+      8: accessForm.add_sensors.mobile || accessForm.add_sensors.web,
+      5: accessForm.alerts.mobile || accessForm.alerts.web,
+      6: accessForm.alert_management.mobile || accessForm.alert_management.web,
+      13: accessForm.dashboard.mobile || accessForm.dashboard.web,
+      4: accessForm.create_trip.mobile || accessForm.create_trip.web,
+      3: accessForm.past_trips.mobile || accessForm.past_trips.web,
+      1: accessForm.home.mobile || accessForm.home.web,
+      15: accessForm.role_settings.mobile || accessForm.role_settings.web,
+      14: accessForm.report.mobile || accessForm.report.web,
+      7: accessForm.sensors.mobile || accessForm.sensors.web,
+      9: accessForm.routes.mobile || accessForm.routes.web,
+      11: accessForm.users.mobile || accessForm.users.web,
+      2: accessForm.trips.mobile || accessForm.trips.web
+    }
 
+    this.dialogRef.close(data);
+  }
 }
